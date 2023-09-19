c15b22825bed1934a71039c3e0c299495c729b1c
walczak-hector <65743857+walczak-hector@users.noreply.github.com>
2023-09-18 21:45:59 -0300

    CSS añadido

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const helmet = require('helmet');
+const path = require('path');
 
 const { DBTest } = require('./database.js');
 const publicacionModel = require('./publicacionModel.js');
@@ -10,13 +11,14 @@ const app = express()
 const PORT = process.env.PORT
 
 // Configurar EJS como motor de plantilla
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async function (req, res) {
     const publicaciones = await publicacionModel.findAll();
